8559e3c459fa7d810d377a9df2a4299d60fdd55b
Arjun Komath <arjunkomath@gmail.com>
2024-08-23 17:33:27 +1000

    Handle duplicate comments

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,31 @@ export default (app: Probot) => {
       const pullRequest = context.payload.pull_request;
       const repo = context.repo();
 
+      console.log(`Handle pull request: ${pullRequest.title}`);
+
+      /**
+       * Delete all previous comments
+       **/
+      // Fetch all review comments
+      const { data: comments } = await context.octokit.pulls.listReviewComments(
+        {
+          ...repo,
+          pull_number: pullRequest.number,
+        }
+      );
+      // Filter comments made by the bot
+      const botComments = comments.filter(
+        (comment) => comment.user?.login === "review-buddy-dev[bot]"
+      );
+      console.log("Deleting bot comments...", botComments.length);
+      // Delete each bot comment
+      for (const comment of botComments) {
+        await context.octokit.pulls.deleteReviewComment({
+          ...repo,
+          comment_id: comment.id,
+        });
+      }
+
       // Fetch the pull request files
       const files = await context.octokit.pulls.listFiles({
         ...repo,
@@ -21,14 +46,15 @@ export default (app: Probot) => {
       // Analyze the changes (you'll implement your AI logic here)
       const aiReviewComments = await analyzeChanges(files.data);
 
-      // Post comments on the pull request
-      await context.octokit.pulls.createReview({
-        ...repo,
-        pull_number: pullRequest.number,
-        body: "ReviewBuddy Comments",
-        event: "COMMENT",
-        comments: aiReviewComments,
-      });
+      for (const comment of aiReviewComments) {
+        await context.octokit.pulls.createReviewComment({
+          owner: context.repo().owner,
+          repo: context.repo().repo,
+          pull_number: pullRequest.number,
+          commit_id: context.payload.pull_request.head.sha,
+          ...comment,
+        });
+      }
     }
   );
 };
@@ -46,18 +72,18 @@ interface FileChange {
   patch?: string;
 }
 
-async function analyzeChanges(files: FileChange[]): Promise<any[]> {
+async function analyzeChanges(files: FileChange[]): Promise<
+  {
+    body: string;
+    path: string;
+    line: number;
+  }[]
+> {
   const comments = [];
 
   for (const file of files) {
-    console.log(`Analyzing file: ${file.filename}`);
-    console.log(`Status: ${file.status}`);
-    console.log(`Additions: ${file.additions}, Deletions: ${file.deletions}`);
-
+    console.log(`Analyzing file: ${file.filename}`, file.patch);
     if (file.patch) {
-      console.log("Patch:");
-      console.log(file.patch);
-
       const aiSuggestions = await analyzePathWithAI(file.patch);
       if (!aiSuggestions?.length) {
         continue;
@@ -71,7 +97,7 @@ async function analyzeChanges(files: FileChange[]): Promise<any[]> {
         }
         comments.push({
           path: file.filename,
-          position: 1, // suggestion.position,
+          line: suggestion.line,
           body: suggestion.message,
         });
       }
@@ -83,7 +109,7 @@ async function analyzeChanges(files: FileChange[]): Promise<any[]> {
 
 const Comment = z.object({
   message: z.string().nullable(),
-  position: z.number(),
+  line: z.number(),
 });
 const ReviewComments = z.object({
   suggestions: z.array(Comment),
@@ -92,7 +118,7 @@ const ReviewComments = z.object({
 async function analyzePathWithAI(patch: string): Promise<
   | {
       message: string | null;
-      position: number;
+      line: number;
     }[]
   | null
 > {
@@ -103,11 +129,17 @@ async function analyzePathWithAI(patch: string): Promise<
       {
         role: "user",
         content: `You are an AI code reviewer and you are given a patch of a file in a GitHub pull request.
-          Please review the patch and provide review comments and the relative position from start in the diff where you want to add a review comment.
-          You can suggest improvements, point out issues, or provide feedback and you can also provide the changes you want to see in the patch.
+          Please review the patch and provide review comments and line number for review comment.
+
+          Guidelines for review comments:
+          You can suggest improvements, point out issues, or provide feedback.
           While providing suggestion, it would be helpful if you can provide the code changes you want to see in the patch.
           You can return markdown formatted message.
           If you don't have any suggestion you can return message as null.
+
+          Guidelines for line:
+          Line is the line number in the file where the comment should be placed.
+          The comment should be placed on the line where the code change is made.
           
           Here is the patch:
           ${patch}`,
